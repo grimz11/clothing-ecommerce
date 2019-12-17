b6cfeb79e73b61124b67e6bc27555c5da9822c82
grimz11 <andogreymark@gmail.com>
2019-12-17 10:38:21 +0800

    Updates

---
+++ precache-manifest.9c8235e7e614d94eeb2c1d8fca274b77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3976168b7e826be848c739e154f53979",
+    "url": "/clothing-ecommerce/index.html"
+  },
+  {
+    "revision": "c7135e023133c3bc87d5",
+    "url": "/clothing-ecommerce/static/css/main.1752def4.chunk.css"
+  },
+  {
+    "revision": "34d66d77a2a0921eccab",
+    "url": "/clothing-ecommerce/static/js/2.efb23470.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/clothing-ecommerce/static/js/2.efb23470.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7135e023133c3bc87d5",
+    "url": "/clothing-ecommerce/static/js/main.c68d002e.chunk.js"
+  },
+  {
+    "revision": "c4859850eee8d02d789d",
+    "url": "/clothing-ecommerce/static/js/runtime-main.7d09557c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing-ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
