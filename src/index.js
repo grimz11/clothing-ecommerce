06c97153c080cd3b31a17c2239f4571e31511d5c
grimz11 <andogreymark@gmail.com>
2019-12-17 10:06:58 +0800

    use github pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import {PersistGate} from 'redux-persist/integration/react';
 
